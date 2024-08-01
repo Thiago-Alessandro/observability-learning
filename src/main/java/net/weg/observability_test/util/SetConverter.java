dea6639d8e13abaa55fa802042ac5fa47a917053
Thiago Alessandro Batista <alebatistathiago@gmail.com>
2024-08-01 15:42:29 -0300

    reafator: light refactor on docker-compose.yml and .env files

--- src/main/java/net/weg/observability_test/util/SetConverter.java
+++ src/main/java/net/weg/observability_test/util/SetConverter.java
@@ -8,20 +8,14 @@ import java.util.stream.Collectors;
 public class SetConverter {
 
     public static Set<String> convertStringToSet(String str) {
-//        System.out.println("String recebida: " + str); // Imprime a string recebida
-        if (str == null || str.isEmpty()) {
-            return new HashSet<>();
-        }
+        if (str == null || str.isEmpty()) return new HashSet<>();
         Set<String> result = Arrays.stream(str.split(","))
                 .collect(Collectors.toSet());
-//        System.out.println("Set resultante: " + result); // Imprime o resultado do Set
         return result;
     }
 
     public static String convertSetToString(Set<String> set) {
-        if (set == null || set.isEmpty()) {
-            return "";
-        }
+        if (set == null || set.isEmpty()) return "";
         return set.stream().collect(Collectors.joining(","));
     }
 
